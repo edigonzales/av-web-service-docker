d136afcc1cd1a90679bb2923d412436448db2469
edigonzales <edi.gonzales@gmail.com>
2026-05-16 20:43:56 +0200

    vite: define base path

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: "/app/",  
   define: {
     'import.meta.env.VITE_MOCK_XML_BASE_PATH': JSON.stringify('/mock-data'),
   },
